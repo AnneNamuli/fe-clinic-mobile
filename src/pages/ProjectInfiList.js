bbc4c547a360d09a9b5a273937c8bef7686a5bf4
annenamuli <anne.namuli@gmail.com>
2021-06-10 11:06:48 +0200

    View for all patient details and delete appointment record

--- src/pages/ProjectInfiList.js
+++ src/pages/ProjectInfiList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -14,8 +14,16 @@ import ConfirmDelete from '../common/ConfirmDelete';
 import DeleteIcon from '@material-ui/icons/Delete';
 import EditIcon from '@material-ui/icons/Edit';
 import AddIcon from '@material-ui/icons/Add'
+import { createBrowserHistory } from "history";
+
+
 import format from 'string-format';
 import PageBase from './PageBase';
+import axios from "axios";
+
+const history = createBrowserHistory({ forceRefresh: true });
+
+
 
 const CustomTableCell = withStyles(theme => ({
   head: {
@@ -38,49 +46,46 @@ const styles = theme => ({
   }
 });
 
-const projectdata = [
-  {
-    SrNo: 1, ProjectName: "ABC", Subject: "Computer Programming", Date: "12/12/2018", Type: "Difficult normal easy"
-  },
-  {
-    SrNo: 2, ProjectName: "XYZ", Subject: "Digital Systems", Date: "12/13/2018", Type: "Normal"
-  },
-  {
-    SrNo: 3, ProjectName: "QWE", Subject: "Data Structures", Date: "12/14/2018", Type: "Easy"
-  },
-  {
-    SrNo: 4, ProjectName: "ASD", Subject: "Software Engineering", Date: "12/15/2018", Type: "Difficult"
-  },
-  {
-    SrNo: 5, ProjectName: "ZXC", Subject: "Java Technologies", Date: "12/16/2018", Type: "Normal"
-  }
-]
-
 
 export class ProjectInfiList extends React.Component {
+
   constructor(props) {
     super(props);
-    this.state = { open: false, deleteRecord: false, id: '', pgNo: 1, pageSize: 10, notify: false, message: '', error: false }
+    this.state = { bookings: null, open: false, deleteRecord: false, id: '', pgNo: 1, pageSize: 10, notify: false, message: '', error: false }
     this.deleteProjectInfo = this.deleteProjectInfo.bind(this);
     this.deleteAfterConfirmation = this.deleteAfterConfirmation.bind(this);
     this.editProjectInfo = this.editProjectInfo.bind(this);
     this.addProjectInfo = this.addProjectInfo.bind(this);
   }
 
+  async componentDidMount(){
+    const headers = {headers: { Authorization: "Bearer " + sessionStorage.getItem("token")} };
+    const url = `http://localhost/api/v1/patient/Bookings`;
+    const response = await axios.get(url, headers)
+    this.setState({bookings: response.data})
+
+  }
+
   deleteProjectInfo(id) {
+    const headers = {headers: { Authorization: "Bearer " + sessionStorage.getItem("token")} };
     this.setState({ deleteRecord: true, id: id });
+    axios.delete(`http://localhost/api/v1/patient/${id}`, headers)
   }
   editProjectInfo(id) {
-    this.props.history.push(`/ProjectInfo/` + id);
+    history.push({pathname: `/ProjectInfo`});
   }
   deleteAfterConfirmation(deleteConfirmed) {
     this.setState({ deleteRecord: false });
     if (deleteConfirmed) {
       /* let selectedid =  this.state.id
         Create and call deleteProjectInfo action */
-        this.showNotification("Deleted!! Create and call deleteProjectInfo action");
+        this.showNotification("Appointment has been deleted");
+        this.deleteProjectInfo(this.state.id)
     }
     this.setState({ id: '' });
+    history.push({
+      pathname: `/ProjectInfiList`,
+    });
   }
   showNotification = (msg, err) => {
     if (err)
@@ -100,41 +105,51 @@ export class ProjectInfiList extends React.Component {
   }
   render() {
     const { classes } = this.props;
+
+    console.log('>>>>>>>>>>>>', this.props)
     const { notify, message, error } = this.state;
 
+
+    console.log('<<<>>>>', this.state)
+    const getBookings = this.state.bookings
+
     return (
-      <PageBase title="List of Project" navigation="Project / Project List">
+      <PageBase title="Appointments" navigation="Project / Project List">
         <Button variant="fab" mini color="primary" aria-label="addd" className={classes.button} onClick={this.addProjectInfo}>
           <AddIcon />
         </Button>
         <Table>
           <TableHead>
             <TableRow>
-              <CustomTableCell>SrNo</CustomTableCell>
-              <CustomTableCell>Project Name</CustomTableCell>
-              <CustomTableCell>Subject</CustomTableCell>
-              <CustomTableCell>Date</CustomTableCell>
-              <CustomTableCell>Type</CustomTableCell>
+              <CustomTableCell>id</CustomTableCell>
+              <CustomTableCell>Patient Name</CustomTableCell>
+              <CustomTableCell>Date of Birth</CustomTableCell>
+              <CustomTableCell>Patent's Contact</CustomTableCell>
+              <CustomTableCell>Clinic Name</CustomTableCell>
+              <CustomTableCell>Clinic address</CustomTableCell>
+              <CustomTableCell>Appointment Date</CustomTableCell>
               <CustomTableCell>Edit</CustomTableCell>
               <CustomTableCell>Delete</CustomTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {projectdata && projectdata.map(projectInfo => {
+            {getBookings && getBookings.map(bookings => {
               return (
-                <TableRow className={classes.row} key={projectInfo.SrNo}>
-                  <CustomTableCell>{projectInfo.SrNo}</CustomTableCell>
-                  <CustomTableCell>{projectInfo.ProjectName}</CustomTableCell>
-                  <CustomTableCell>{projectInfo.Subject}</CustomTableCell>
-                  <CustomTableCell>{projectInfo.Date}</CustomTableCell>
-                  <CustomTableCell>{projectInfo.Type}</CustomTableCell>
+                <TableRow className={classes.row} key={bookings.id}>
+                  <CustomTableCell>{bookings.id}</CustomTableCell>
+                  <CustomTableCell>{bookings.full_name}</CustomTableCell>
+                  <CustomTableCell>{bookings.date_of_birth}</CustomTableCell>
+                  <CustomTableCell>{bookings.phone_number}</CustomTableCell>
+                  <CustomTableCell>{bookings.name}</CustomTableCell>
+                  <CustomTableCell>{bookings.address}</CustomTableCell>
+                  <CustomTableCell>{bookings.appointment_date}</CustomTableCell>
                   <CustomTableCell>
-                    <Button variant="fab" mini aria-label="edit" className={classes.button} onClick={() => this.editProjectInfo(projectInfo.SrNo)}>
+                    <Button variant="fab" mini aria-label="edit" className={classes.button} onClick={() => this.editProjectInfo(bookings.id)}>
                       <EditIcon />
                     </Button>
                   </CustomTableCell>
                   <CustomTableCell>
-                    <Button variant="fab" mini color="secondary" aria-label="delete" className={classes.button} onClick={() => this.deleteProjectInfo(projectInfo.SrNo)} >
+                    <Button variant="fab" mini color="secondary" aria-label="delete" className={classes.button} onClick={() => this.deleteProjectInfo(bookings.id)} >
                       <DeleteIcon />
                     </Button>
                   </CustomTableCell>
@@ -143,7 +158,7 @@ export class ProjectInfiList extends React.Component {
             })}
           </TableBody>
         </Table>
-        <ConfirmDelete resourceHeader="Delete Project Information ?" resourceSubject={format("Do you want to delete project information '{}'?", this.state.id)} onModalClose={this.deleteAfterConfirmation}
+        <ConfirmDelete resourceHeader="Delete Appointment ?" resourceSubject={format("Do you want to delete appointment for ID '{}'?", this.state.id)} onModalClose={this.deleteAfterConfirmation}
           openDeleteDialog={this.state.deleteRecord} />
 
         <Notification
@@ -157,11 +172,10 @@ export class ProjectInfiList extends React.Component {
   }
 }
 
+
 ProjectInfiList.propTypes = {
   classes: PropTypes.object.isRequired,
 };
-
-
 
 
 export default withStyles(styles)(ProjectInfiList);
